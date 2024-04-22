1ba36a397680821ae9414c8889317ab99f03d16a
Tom Keim <tom@tomkeim.nl>
2024-04-22 16:23:03 +0200

    Update zehnder.cpp

--- components/zehnder/zehnder.cpp
+++ components/zehnder/zehnder.cpp
@@ -306,7 +306,8 @@ void ZehnderRF::rfHandleReceived(const uint8_t *const pData, const uint8_t dataL
             (void) memset(this->_txFrame, 0, FAN_FRAMESIZE);  // Clear frame data
 
             pTxFrame->rx_type = FAN_TYPE_MAIN_UNIT;  // Set type to main unit
-            pTxFrame->rx_id = 0x00;  // Broadcast
+            //pTxFrame->rx_id = 0x00;  // Broadcast
+            pTxFrame->rx_id = pResponse->tx_id; // Set ID to the ID of the main unit
             pTxFrame->tx_type = this->config_.fan_my_device_type;
             pTxFrame->tx_id = this->config_.fan_my_device_id;
             pTxFrame->ttl = FAN_TTL;
